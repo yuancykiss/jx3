2d9fbbc94aa174625cc670b2fe0479d9c4d58bf7
jksyue <1186921650@qq.com>
2018-11-03 15:11:38 +0800

    定义服务器model

--- qiyu/models.py
+++ qiyu/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class P_Server(models.Model):
+    class Meta():
+        db_table = 'p_server'
+
+    server_id = models.IntegerField(null=False, unique=True, primary_key=True, )
+    area_name = models.CharField(max_length=50, null=True, default='')
+    server_name = models.CharField(max_length=50, null=True, default='')
+
