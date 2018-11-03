1099951f3dfd8dd9632ce7fcf6a9b362f60dffbb
jksyue <1186921650@qq.com>
2018-11-03 14:56:16 +0800

    index 页面

--- jx3/urls.py
+++ jx3/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
     url(r'^qiyu/', include('qiyu.urls', namespace='qiyu')),
-    
+
 ]
--- qiyu/urls.py
+++ qiyu/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from qiyu.views import index
+# from qiyu.views import index
+from qiyu import views
 
 urlpatterns = [
     # 欢迎界面
-    url(r'^admin/', index, name='index'),
+    url(r'^index/', views.index, name='index'),
 ]
